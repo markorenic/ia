f40b8130f48c8fdd6aa9f8a44133a888314b176e
Marko Renic <marko.renic@gmail.com>
2019-03-27 16:31:12 -0500

    Homepage added timeline and other simple changes to styles + added bootstrap

--- php/homepage.php
+++ php/homepage.php
@@ -1,22 +1,54 @@
 <header>
     <link rel="stylesheet" href="style/homepage.css">
+    <link rel = "stylesheet" href = "style/bootstrap.css">
+    <link rel = "stylesheet" href = "style/style.css">
 </header>
 
-<body class="parallax"> 
-<div class="flex-container">
-<div class="flex-box box"> I am a new thingy 1 I am a new thingy 1 I am a new thingy 1 I am a new thingy 1 I am a new thingy 1I am a new thingy 1 I am a new thingy 1 I am a new thingy 1 I am a new thingy 1 I am a new thingy 1I am a new thingy 1 I am a new thingy 1 I am a new thingy 1 I am a new thingy 1 I am a new thingy 1I am a new thingy 1 I am a new thingy 1 I am a new thingy 1 I am a new thingy 1 I am a new thingy 1I am a new thingy 1 I am a new thingy 1 I am a new thingy 1 I am a new thingy 1 I am a new thingy 1I am a new thingy 1 I am a new thingy 1 I am a new thingy 1 I am a new thingy 1 I am a new thingy 1I am a new thingy 1 I am a new thingy 1 I am a new thingy 1 I am a new thingy 1 I am a new thingy 1I am a new thingy 1 I am a new thingy 1 I am a new thingy 1 I am a new thingy 1 I am a new thingy 1I am a new thingy 1 I am a new thingy 1 I am a new thingy 1 I am a new thingy 1 I am a new thingy 1I am a new thingy 1 I am a new thingy 1 I am a new thingy 1 I am a new thingy 1 I am a new thingy 1I am a new thingy 1 I am a new thingy 1 I am a new thingy 1 I am a new thingy 1 I am a new thingy 1I am a new thingy 1 I am a new thingy 1 I am a new thingy 1 I am a new thingy 1 I am a new thingy 1I am a new thingy 1 I am a new thingy 1 I am a new thingy 1 I am a new thingy 1 I am a new thingy 1I am a new thingy 1 I am a new thingy 1 I am a new thingy 1 I am a new thingy 1 I am a new thingy 1I am a new thingy 1 I am a new thingy 1 I am a new thingy 1 I am a new thingy 1 I am a new thingy 1</div>
-<div class="flex-box box"> I am a new thingy 2I am a new thingy 2I am a new thingy 2I am a new thingy 2</div>
-<div class="flex-box box"> I am a new thingy 3</div>
-<div class="flex-box box"> I am a new thingy 1</div>
-<div class="flex-box box"> I am a new thingy 2</div>
-<div class="flex-box box"> I am a new thingy 3</div>
+<body class="parallax">
+<div class = "container">
+            <div class = "row">
+                <div class = "col-md-6 left">
+                        <div class = "row">
+                            <div class = "items">
+                            <img class="image" src="resources/test.jpg">
+                            </div>
+                            <div class = "items">
+                                 Checking out the Facebook Timeline Design.
+                            </div>
+                            <div class = "items">
+                                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+                            </div>
+                            <div class = "items">
+                                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+                            </div>
+                            <div class = "items">
+                                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+                            </div>
+                            <div class = "items">
+                                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+                            </div>
+                            <div class = "items">
+                                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+                            </div>
+                        </div>
+                </div>
 
-
-
-
-
-
-
-
-</div>
+                <div class = "col-md-6 right">
+                        <div class = "row">
+                            <div class = "items">
+                                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+                            </div>
+                            <div class = "items">
+                                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+                            </div>
+                            <div class = "items">
+                                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+                            </div>
+                            <div class = "items">
+                                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+                            </div>
+                        </div>
+                </div>
+            </div>
+        </div>
 </body>
